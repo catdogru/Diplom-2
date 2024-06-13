457c8739717a4f7d9a8abee7697f2f770001de75
daria.i <dvigina@yandex.ru>
2024-06-14 02:01:18 +0300

    review

--- src/main/java/ru/yandex/stellar/burgers/Constants.java
+++ src/main/java/ru/yandex/stellar/burgers/Constants.java
@@ -2,57 +2,57 @@ package ru.yandex.stellar.burgers;
 
 import static org.apache.commons.lang3.RandomStringUtils.randomNumeric;
 
-public interface Constants {
-    String STELLAR_BURGERS_BASE_URI = "https://stellarburgers.nomoreparties.site";
-    String API_PATH = "/api";
+public class Constants {
+    public static final String STELLAR_BURGERS_BASE_URI = "https://stellarburgers.nomoreparties.site";
+    public static final String API_PATH = "/api";
 
     // common json keys
-    String SUCCESS_JSON_KEY = "success";
-    String MESSAGE_JSON_KEY = "message";
+    public static final String SUCCESS_JSON_KEY = "success";
+    public static final String MESSAGE_JSON_KEY = "message";
 
     // common messages
-    String UNAUTHORIZED_USER_MESSAGE = "You should be authorised";
+    public static final String UNAUTHORIZED_USER_MESSAGE = "You should be authorised";
 
-    interface UserConstants {
+    public static class UserConstants {
         // api path
-        String CREATE_USER_PATH = API_PATH + "/auth/register";
-        String LOGIN_USER_PATH = API_PATH + "/auth/login";
-        String UPDATE_USER_PATH = API_PATH + "/auth/user";
-        String DELETE_USER_PATH = API_PATH + "/auth/user";
+        public static final String CREATE_USER_PATH = API_PATH + "/auth/register";
+        public static final String LOGIN_USER_PATH = API_PATH + "/auth/login";
+        public static final String UPDATE_USER_PATH = API_PATH + "/auth/user";
+        public static final String DELETE_USER_PATH = API_PATH + "/auth/user";
 
         // response message
-        String USER_ALREADY_EXISTS_MESSAGE = "User already exists";
-        String EMPTY_REQUIRED_FIELD_MESSAGE = "Email, password and name are required fields";
-        String USER_REMOVED_MESSAGE = "User successfully removed";
-        String INCORRECT_CREDENTIALS_MESSAGE = "email or password are incorrect";
-        String EMAIL_ALREADY_IN_USE_MESSAGE = "User with such email already exists";
+        public static final String USER_ALREADY_EXISTS_MESSAGE = "User already exists";
+        public static final String EMPTY_REQUIRED_FIELD_MESSAGE = "Email, password and name are required fields";
+        public static final String USER_REMOVED_MESSAGE = "User successfully removed";
+        public static final String INCORRECT_CREDENTIALS_MESSAGE = "email or password are incorrect";
+        public static final String EMAIL_ALREADY_IN_USE_MESSAGE = "User with such email already exists";
 
         //data
-        String DEFAULT_PASSWORD = "Qwerty12345";
-        String DEFAULT_USER_NAME = "Тестовый Тест Тестович";
+        public static final String DEFAULT_PASSWORD = "Qwerty12345";
+        public static final String DEFAULT_USER_NAME = "Тестовый Тест Тестович";
 
-        static String generateRandomEmail() {
+        public static String generateRandomEmail() {
             return "testEmail_" + randomNumeric(4) + "@yandex.ru";
         }
     }
 
-    interface OrderConstants {
+    public static class OrderConstants {
         // api path
-        String CREATE_ORDER_PATH = API_PATH + "/orders";
-        String GET_ORDERS_PATH = API_PATH + "/orders";
+        public static final String CREATE_ORDER_PATH = API_PATH + "/orders";
+        public static final String GET_ORDERS_PATH = API_PATH + "/orders";
 
         // response message
-        String INGREDIENT_MUST_BE_PROVIDED_MESSAGE = "Ingredient ids must be provided";
+        public static final String INGREDIENT_MUST_BE_PROVIDED_MESSAGE = "Ingredient ids must be provided";
 
         // data
-        String INVALID_INGREDIENT = "INVALID_INGREDIENT";
+        public static final String INVALID_INGREDIENT = "INVALID_INGREDIENT";
     }
 
-    interface IngredientConstants {
+    public static class IngredientConstants {
         // api path
-        String GET_INGREDIENTS_PATH = API_PATH + "/ingredients";
+        public static final String GET_INGREDIENTS_PATH = API_PATH + "/ingredients";
 
         // json keys
-        String DATA_JSON_KEY = "data";
+        public static final String DATA_JSON_KEY = "data";
     }
 }
